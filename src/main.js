80da4f3358638d246299a1fbd3638d01584109ca
yuKalasnikova2 <pmalian@list.ru>
2024-01-18 12:46:30 +0300

    initiall firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import { createApp } from 'vue'
+import { initializeApp } from "firebase/app"
 import './style.css'
 import App from './App.vue'
 import router from './router'
 
+const firebaseConfig = {
+    apiKey: "AIzaSyDSYW4TBVOROkwj62hhZPbkInamr6-BMZs",
+    authDomain: "travalizer-users.firebaseapp.com",
+    projectId: "travalizer-users",
+    storageBucket: "travalizer-users.appspot.com",
+    messagingSenderId: "753383881117",
+    appId: "1:753383881117:web:ed5cb2fe40f11e9ef75774"
+  }
+  initializeApp(firebaseConfig)
+
+
 createApp(App).use(router).mount('#app')
+
+
