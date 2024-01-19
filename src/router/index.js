8ed33f9196cbbdaf5d6c074ba9dc77283e213cf6
yuKalasnikova2 <pmalian@list.ru>
2024-01-19 09:16:13 +0300

    feat(routes): navigation guards

--- src/router/index.js
+++ src/router/index.js
@@ -5,50 +5,46 @@ import ListItem from '../views/ListItem.vue'
 import SignIn from '../views/SignIn.vue'
 import SignUp from '../views/SignUp.vue'
 
+const isAuthenticated = localStorage.getItem('isAuthenticated')
+
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Home,
-        },
-        {
-            path: '/list',
-            name: 'list',
-            component: List,
-            meta: {
-                enter: true
-            }
-        },
-        {
-            path: '/list/:idcard',
-            name: 'listitem',
-            component: ListItem,
-        },
-        {
-            path: '/signin',
-            name: 'signin',
-            component: SignIn,
-        },
-        {
-            path: '/signup',
-            name: 'signup',
-            component: SignUp,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: List,
 
-        },
-        
-
-    ]
+      beforeEnter: (to, from, next) => {
+        if (!isAuthenticated) {
+          next('/signin')
+        } else {
+          next()
+        }
+        return false
+      },
+    },
+    {
+      path: '/list/:idcard',
+      name: 'listitem',
+      component: ListItem,
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: SignIn,
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUp,
+    },
+  ],
 })
 
-// router.beforeEach((to, from, next) => {
-//     const isAuthenticated = localStorage.getItem('isAuthenticated');
-  
-//     if (to.meta.enter && isAuthenticated === '' && to.name !== 'signin') {
-//       next('/signin')
-//     } else {
-//       next('');
-//     }
-//   });
 export default router
